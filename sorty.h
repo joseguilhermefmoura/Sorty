289b689bf580e00ec9fcc5e4d7b69a0b20e5f8f8
joseguilhermefmoura <joseguilhermefmoura@gmail.com>
2020-08-27 15:19:18 -0300

    Added Bubble Sort and Insertion Sort

--- sorty.h
+++ sorty.h
@@ -1,8 +1,10 @@
 typedef int Item;
+int BUBBLE_COUNT = 0;
+
 #define key(A) (A)
 #define less(A, B) (A < B)
-#define exch(A, B) {Item T = A; A = B; B = T;}
-
+#define exch(A, B) {Item T = A; A = B; B = T; BUBBLE_COUNT = BUBBLE_COUNT + 1;}
+#define cmpexch(A, B) {if(B < A) exch(A, B);}
 
 void selection_sort_r(Item *v, int l, int r){
     /* Recursive Solution */
@@ -29,4 +31,44 @@ void selection_sort(Item *v, int l, int r){
         exch(v[min], v[l]);
         l++;
     }
+}
+
+void bubble_sort(Item *v, int l, int r){
+    BUBBLE_COUNT = 0;
+    int i = 0;
+    while(i < r){
+        for(int j = l; j < r; j++){
+            cmpexch(v[j], v[j + 1]);
+        }
+        if (BUBBLE_COUNT == 0){
+            return;
+        }
+        i++;
+    }
+}
+
+void insertion_sort(Item *v, int l, int r){
+
+    for(int i = l + 1; i <= r; i++){
+        for(int j = i; j > l; j--){
+            cmpexch(v[j - 1], v[j]);
+        }
+    }
+}
+void insertion_sort_v2(Item *v, int l, int r){
+    // Reordenate the array so the first element is the minor one.
+    for(int i = r; i > l; i--){
+        cmpexch(v[i - 1], v[i]);
+    }
+    
+    for(int i = l + 2; i <= r; i++){
+        int j = i;
+        Item tmp = v[j];
+        while(less(tmp, v[j - 1])){
+            v[j] = v[j - 1];
+            j--;
+        }
+        v[j] = tmp;
+    }
+
 }
