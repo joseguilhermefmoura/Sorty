81f1bba0e7c675dddf2257e083ce6eb01a9e5d54
joseguilhermefmoura <joseguilhermefmoura@gmail.com>
2020-09-03 18:02:48 -0300

    Adding build

---
+++ docs/build/precache-manifest.b94cbf38167dcbd00e8ffce3eff76f6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6678b1c7932d3c84f4563f2022961239",
+    "url": "/Sorty/index.html"
+  },
+  {
+    "revision": "391bf3cde143466cc6e6",
+    "url": "/Sorty/static/css/main.42c80541.chunk.css"
+  },
+  {
+    "revision": "9c3879c08972192252b9",
+    "url": "/Sorty/static/js/2.7f3d57d0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Sorty/static/js/2.7f3d57d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "391bf3cde143466cc6e6",
+    "url": "/Sorty/static/js/main.ee7dfae2.chunk.js"
+  },
+  {
+    "revision": "1ff5fa675c8ef93122af",
+    "url": "/Sorty/static/js/runtime-main.439d736b.js"
+  }
+]);
